86d491a8b4c0d40d86328708d09f9b9258951046
Ivan Razin <mozaroc@gmail.com>
2023-06-06 13:09:25 +0300

    Create nextcloud.sh

---
+++ nextcloud.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -xe
+
+curl -fsSL https://get.docker.com | sudo sh
+
+docker run --sig-proxy=false --name nextcloud-aio-mastercontainer --restart always --publish 80:80 --publish 8080:8080 --publish 8443:8443 --volume nextcloud_aio_mastercontainer:/mnt/docker-aio-config --volume /var/run/docker.sock:/var/run/docker.sock:ro nextcloud/all-in-one:latest
+
+exit 0
